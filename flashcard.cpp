11c1df1d2a2d646193365fe37dc75bac55fc2182
Algec-bdu <jobayer0001@std.bdu.ac.bd>
2023-11-03 02:17:17 +0600

    Added custom stack, queue and priority class. Implemented Undo functionality for flashcards

--- flashcard.cpp
+++ flashcard.cpp
@@ -1,7 +1,5 @@
 #include <cstdlib>
 #include <iostream>
-#include <queue>
-#include <stack>
 #include <string>
 #include <vector>
 using namespace std;
@@ -20,16 +18,112 @@ class FlashCard {
       this->weight = 0;
     }
     bool operator<(const FlashCard& other) const {
-      // You can define your own comparison logic here.
-      // For example, compare based on 'front' or 'back' strings.
       return weight < other.weight;
     }
+    bool operator>(const FlashCard& other) const {
+      return weight > other.weight;
+    }
 };
+
+class PriorityQueue {
+    vector<FlashCard> heap;
+public:
+    void push(FlashCard data) {
+        heap.push_back(data);
+        int n = heap.size();
+        upHeapify(n - 1);
+    }
+    void pop() {
+        swap(heap[0], heap[heap.size() - 1]);
+        heap.pop_back();
+        downHeapify(0);
+    }
+    FlashCard top() {
+        return heap[0];
+    }
+    bool empty() {
+        return heap.empty();
+    }
+    int size() const{
+      return heap.size();
+    }
+private:
+    void upHeapify(int i) {
+        int parent = (i - 1) / 2;
+        if (heap[i] > heap[parent]) {
+            swap(heap[i], heap[parent]);
+            upHeapify(parent);
+        }
+    }
+    void downHeapify(int i) {
+        int child = 2 * i + 1;
+        if (child >= heap.size()) return;
+        if (heap[child] < heap[child + 1]) child++;
+        if (heap[i] < heap[child]) {
+            swap(heap[i], heap[child]);
+            downHeapify(child);
+        }
+    }
+};
+
+class Stack {
+    private:
+        vector<FlashCard> stack;
+    public:
+        void push(FlashCard flashcard) {
+            stack.push_back(flashcard);
+        }
+        bool pop() {
+            if (isEmpty()) {
+                return false;
+            }
+            stack.pop_back();
+            return true;
+        }
+        FlashCard peek() {
+            return stack.back();
+        }
+        bool isEmpty() {
+            return stack.empty();
+        }
+        int size() {
+            return stack.size();
+        }
+};
+
+
+template <typename T>
+class queue {
+private:
+    std::vector<T> queue;
+public:
+    void push(const T& value) {
+        queue.push_back(value);
+    }
+    void pop() {
+        if (!queue.empty()) {
+            queue.erase(queue.begin());
+        }
+    }
+    T front() {
+        if (!queue.empty()) {
+            return queue.front();
+        }
+        throw std::runtime_error("Queue is empty");
+    }
+    bool empty() {
+        return queue.empty();
+    }
+    int size() {
+        return queue.size();
+    }
+};
+
 
 class Group {
   public:
     string name;
-    priority_queue<FlashCard> group;
+    PriorityQueue group;
     Group *next;
     Group (string g) {
       this->name = g;
@@ -132,7 +226,7 @@ public:
 };
 
 GroupList group_list;
-stack<FlashCard> record;
+Stack record;
 
 // ***************
 
@@ -160,12 +254,12 @@ void mainMenu() {
 
   while (true) {
     system("clear");
-    cout << "Main Menu:" << endl;
+    cout << "Main Menu:" << endl << endl;
     cout << "1. Open a group" << endl;
     cout << "2. Create a group" << endl;
     cout << "3. Display all groups" << endl;
     cout << "4. Delete a group" << endl;
-    cout << "5. Exit" << endl;
+    cout << "5. Exit" << endl << endl;
 
     // Prompt the user for an option.
     cout << "Select an option: ";
@@ -198,6 +292,7 @@ void mainMenu() {
 // Create group list then select one to open
 void openGroup() {
   system("clear");
+  cout << "~~~~~~~~~~ Open Group ~~~~~~~~~~" << endl << endl;;
   int op;
   group_list.traverse();
   cout << endl << "Enter group number to open (-1 to return): ";
@@ -209,6 +304,7 @@ void openGroup() {
 // Create a new word group
 void createGroup() {
   system("clear");
+  cout << "~~~~~~~~~~ Create Group ~~~~~~~~~~" << endl << endl;;
   string name;
   cout << "Enter new group name: ";
   cin >> name;
@@ -218,15 +314,18 @@ void createGroup() {
 // print all groups
 void displayAllGroups() {
   system("clear");
+  cout << "~~~~~~~~~~ Display Group ~~~~~~~~~~" << endl << endl;;
   int op;
   group_list.display();
-  cout << "Enter any number to return: ";
-  cin >> op;
+  cout << "Enter to return: ";
+  getchar();
+  getchar();
 }
 
 // delete a single group
 void deleteGroup() {
   system("clear");
+  cout << "~~~~~~~~~~ Delete Group ~~~~~~~~~~" << endl << endl;;
   int op;
   group_list.traverse();
   cout << "Which one want to delete?(-1 to return): ";
@@ -251,7 +350,7 @@ void groupMenu(Group* g) {
     cout << "2. Delete a flashcard" << endl;
     cout << "3. Test a random flashcard" << endl;
     cout << "4. Show all flashcard" << endl;
-    cout << "5. Exit" << endl;
+    cout << "5. Exit" << endl << endl;
 
     // Prompt the user for an option.
     cout << "Select an option: ";
@@ -284,18 +383,38 @@ void groupMenu(Group* g) {
 // create new card for group
 void addFlashCard(Group *g) {
   system("clear");
-  string front, rear;
-  cout << "Enter front part: ";
-  cin >> front;
-  cout << "Enter rear part: ";
-  cin.ignore();
-  getline(cin, rear);
-  g->group.push(FlashCard(front, rear));
+  cout << "~~~~~ Add Flashcard in " << g->name << " ~~~~~" << endl << endl;
+  int op;
+  cout << "1. Create new flashcard" << endl;
+  cout << "2. Undo flashcard" << endl;
+  cin >> op;
+  if (op == 1) {
+    string front, rear;
+    cout << "Enter front part: ";
+    cin >> front;
+    cout << "Enter rear part: ";
+    cin.ignore();
+    getline(cin, rear);
+    g->group.push(FlashCard(front, rear));
+  }
+  else if (op == 2) {
+    if (!record.isEmpty()) {
+      g->group.push(record.peek());
+      record.pop();
+    }
+    else {
+      cout << "nothing to undo!" << endl;
+      cout << "Enter to return: ";
+      getchar();
+      getchar();
+    }
+  }
 }
 
 // delete a card from group
 void deleteFlashCard(Group *g) {
   system("clear");
+  cout << "~~~~~ Add Flashcard in " << g->name << " ~~~~~" << endl << endl;
   int op, count = 1;
   queue<FlashCard> temp;
   cout << "1. Delete by ID" << endl;
@@ -307,8 +426,9 @@ void deleteFlashCard(Group *g) {
     cin >> id;
     if (id < 1 || id > g->group.size()) {
       cout << "Invalid ID!" << endl;
-      cout << "Enter any number to return: ";
-      cin >> op;
+      cout << "Enter to return: ";
+      getchar();
+      getchar();
       return;
     }
     else {
@@ -343,8 +463,9 @@ void deleteFlashCard(Group *g) {
     }
     if (!found) {
       cout << "Word not found!" << endl;
-      cout << "Enter any number to return: ";
-      cin >> op;
+      cout << "Enter to return: ";
+      getchar();
+      getchar();
     }
   }
   else {
@@ -359,6 +480,7 @@ void deleteFlashCard(Group *g) {
 // test a random card based on weight
 void randomFlashCard(Group *g) {
   system("clear");
+  cout << "~~~~~ Random Flashcard in " << g->name << " ~~~~~" << endl << endl;
   if (g->group.empty()) return;
   string ans;
   FlashCard q = g->group.top();
@@ -377,13 +499,14 @@ void randomFlashCard(Group *g) {
     q.weight += 1;
   }
   g->group.push(q);
-  cout << "Enter any number to return: ";
-  cin >> ans;
+  cout << "Enter to return: ";
+  getchar();
 }
 
 // show front and back of all cards in group
 void displayAllFlashCard(Group *g) {
   system("clear");
+  cout << "~~~~~ All Flashcards in " << g->name << " ~~~~~" << endl << endl;
   queue<FlashCard> temp;
   while (!g->group.empty()) {
     FlashCard f = g->group.top();
@@ -397,8 +520,9 @@ void displayAllFlashCard(Group *g) {
     temp.pop();
   }
   int op;
-  cout << "Enter any number to return: ";
-  cin >> op;
+  cout << "Enter to return: ";
+  getchar();
+  getchar();
 }
 
 // *******************
